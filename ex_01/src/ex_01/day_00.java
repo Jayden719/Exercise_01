048a014ce866df8330bc356b73c6f9c049a335fa
Jayden Park <Jayden Park@Jayden>
2023-07-15 01:19:45 +0900

    연습_00

--- ex_01/src/ex_01/day_00.java
+++ ex_01/src/ex_01/day_00.java
@@ -5,6 +5,8 @@ public class day_00 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, I'm java");
+		System.out.println("Hello, I'm java2");
+		
 				
 	}
 
