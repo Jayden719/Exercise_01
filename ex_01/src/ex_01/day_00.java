6f408eeb9d907ba1692632d156518955c6c0b7f4
Jayden Park <Jayden Park@Jayden>
2023-07-23 11:34:50 +0900

    테스트

--- ex_01/src/ex_01/day_00.java
+++ ex_01/src/ex_01/day_00.java
@@ -6,7 +6,7 @@ public class day_00 {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, I'm java");
 		System.out.println("Hello, I'm java2"); 
-		
+		System.out.println("안녕하세요");		
 				
 	}
 
